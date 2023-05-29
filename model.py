29b22ae6b9b6596832e9a1552db1c9a61bea6234
2jcortesj2 <jjose.cortes@udea.edu.co>
2023-05-29 12:19:17 -0500

    rename

---
+++ model.py
@@ -0,0 +1,21 @@
+import matlab.engine
+
+class Sonido:
+    def __init__(self):
+        self.eng = matlab.engine.start_matlab()
+        self.eng.addpath('Matlab', nargout=0)
+        self.myobj = self.eng.Sound()
+
+    def grabarAudio(self, path):
+        self.eng.recordAudio(self.myobj, path)
+
+    def syncYPromeGrabaciones(self, folderPath, outputFilePath):
+        # Los path tienen que ser carpetas
+        self.eng.synchronizeAndAverageRecordings(self.myobj, folderPath, outputFilePath)
+    
+    def compararFFT(self, path_wav, path_record):
+        return self.eng.compareFFT(self.myobj, path_wav, path_record)
+
+
+class interfaz_metodo:
+    pass
