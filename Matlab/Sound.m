0045022c69e486771aed6089e7327ba06f71206a
2jcortesj2 <jjose.cortes@udea.edu.co>
2023-05-30 15:14:29 -0500

    cositas

--- Matlab/Sound.m
+++ Matlab/Sound.m
@@ -2,11 +2,11 @@ classdef Sound < handle
     properties (Constant)
 
         FRECUENCIA_MUESTREO = 44100;
-        DURACION_GRABACION = 3;
+
     end
     
     methods (Access = public)
-        function audio_data = recordAudio(obj, folderPath, fs)
+        function audio_data = recordAudio(obj, folderPath, fs, write)
             
             recObj = audiorecorder(obj.FRECUENCIA_MUESTREO, 16, 1);
             
@@ -16,8 +16,10 @@ classdef Sound < handle
             % Obtener los datos grabados
             audio_data = getaudiodata(recObj);
             
-            % Guardar los datos grabados en un archivo
-            audiowrite(folderPath, audio_data, obj.FRECUENCIA_MUESTREO);         
+            if write
+                % Guardar los datos grabados en un archivo
+                audiowrite(folderPath, audio_data, obj.FRECUENCIA_MUESTREO);  
+            end
         end
         
 
@@ -32,7 +34,7 @@ classdef Sound < handle
             % Leer cada archivo WAV y almacenar las grabaciones de voz en la celda
             for i = 1:numel(wavFiles)
                 filePath = fullfile(folderPath, wavFiles(i).name);
-                [recording, sampleRate] = audioread(filePath);
+                [recording, ~] = audioread(filePath);
                 recordings{i} = recording;
             end
         end
@@ -64,7 +66,7 @@ classdef Sound < handle
             recordings = obj.readWavFilesFromFolder(folderPath);
 
             % Obtener la duración de las grabaciones
-            maxDuration = obj.DURACION_GRABACION;
+            maxDuration = max(cellfun(@length, recordings));
         
             % Sincronizar las grabaciones y obtener segmentos de interés
             segments = cell(size(recordings));
@@ -93,24 +95,65 @@ classdef Sound < handle
             % Guardar el archivo resultante como .wav
             obj.saveAsWav(averagedFeatures, outputFilePath);
         end
-        
 
-        function distance = compareFFT(obj, path_wav, path_record)
+
+        function trimmedRecording = trimRecording(obj, recording)
+            % Umbral para detectar el fin del silencio
+            threshold = 0.5;
+            
+            % Encontrar el índice donde termina el silencio
+            endIdx = find(abs(recording) > threshold, 1, 'last');
+            
+            % Extraer el segmento de interés a partir del final del silencio
+            trimmedRecording = recording(endIdx+1:end);
+        end
+
+        function DEE = procesing_audio(obj, audio_wav)
+
+            % Se normaliza la amplitud (volumen) de la señal en el tiempo.
+            audio_wav = audio_wav/max(abs(audio_wav)); 
+            
+            % Total de muestras.
+            N = length(audio_wav);
+            
+            % Optimización del algoritmo FFT a través de un vector de 
+            % muestras que sea una potencia de 2.
+            NFFT = 2^nextpow2(N); 
+
+            % Se calcula la transformada de Fourier.
+            Y = fft(audio_wav, NFFT); 
+
+            % Como se va a trabajar con señales reales, se recorta el 
+            % espectro y se trabaja con el espectro unilateral.
+            Y = Y(1:NFFT/2);
+            
+            % Se calcula el módulo de la transformada.
+            Yabs = abs(Y);
+
+            % Densidad espectral de energía de la muestra.
+            DEE = Yabs.^2;
+        end
+
+
+        function error = compareDEE(obj, path_wav, fs)
+
+            % Realizamos la grabacion del audio
+            record_data = obj.recordAudio('C:\Users\Usuario\Desktop\JUAN\FINAL_METODOS\Records\Avareged\a.wav', fs, false);
+
             % Leer el archivo WAV original
-            [audio_wav, fs_wav] = audioread(path_wav);
-        
-            % Leer la grabación de audio desde el archivo WAV de la grabación
-            [audio_record, fs_record] = audioread(path_record);
-        
+            [audio_wav, ~] = audioread(path_wav);
+
+            % Aseguramos que ambas señales tengan la misma longitud
+            sound_data = obj.alignDuration(record_data, length(audio_wav));
+
             % Realizar la Transformada Rápida de Fourier (FFT)
-            fft_audio_wav = fft(audio_wav);
-            fft_audio_record = fft(audio_record);
-        
-            % Calcular la distancia Euclidiana entre los vectores de características
-            distance = norm(fft_audio_wav - fft_audio_record);
-        
-            % Mostrar el resultado de la comparación
-            fprintf('La distancia Euclidiana entre los espectros de frecuencia es: %f\n', distance);
+            DEE_record_data = obj.procesing_audio(sound_data);
+            DEE_audio_wav = obj.procesing_audio(audio_wav);
+
+            error = mean( ...
+                abs(DEE_audio_wav(1:length(DEE_audio_wav)/2) - ...
+                DEE_record_data(1:length(DEE_audio_wav)/2)) ...
+                );
         end
     end
 end
--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import matlab.engine
+from threading import Thread
+import pygame
 
+# Metodos traidos de matlab
 class Sonido:
     def __init__(self):
         # Se inicializa en engine
@@ -10,12 +13,12 @@ class Sonido:
         self.myobj = self.eng.Sound()
 
 
-    def grabarAudio(self, path, fs):
+    def grabarAudio(self, path="", fs=3, write=False):
         r'''
         El path debe de contener el nombre del archivo.
         ej. \ubicacion\nombre.waw
         '''
-        self.eng.recordAudio(self.myobj, path, fs)
+        self.eng.recordAudio(self.myobj, path, fs, write)
 
 
     def syncYPromeGrabaciones(self, folderPath, outputFilePath):
@@ -27,16 +30,14 @@ class Sonido:
         self.eng.synchronizeAndAverageRecordings(self.myobj, folderPath, outputFilePath)
     
 
-    def compararFFT(self, path_wav, path_record):
+    def compararDEE(self, path_wav, fs=1, rango=[0, 1100]):
         r'''
         Va a retornar el resultado numérico de la distancia euclidiana entre las dos funciones
         de fft.
         Es necesario poner el nombre del archivo en la dirección
         ej. \ubicacion\nombre.waw
         '''
-        diferencia = self.eng.compareFFT(self.myobj, path_wav, path_record)
-
-        if 6750 < diferencia < 7150:
+        if rango[0] < self.eng.compareDEE(self.myobj, path_wav, fs) < rango[1]:
             return True
         else:
             return False
@@ -44,6 +45,47 @@ class Sonido:
     def grabarYCompararSonido(self):
         pass
 
+# Clase que crea un hilo para que se ejecute las grabaciones y el análisis en segundo plano
+class SoundRecorder(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.recording = False
+        self.sound = Sonido()
+
+    def run(self):
+        while self.recording:
+            # Process the recorded sound data as needed
+            if self.sound.compararDEE(r'Records\start.wav'):
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_r))
+
+
+            if self.sound.compararDEE(r'Records\up.wav'):
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_UP))
+
+
+            if self.sound.compararDEE(r'Records\down.wav'):
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_DOWN))
+
+
+            if self.sound.compararDEE(r'Records\right.wav'):
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_RIGHT))
+
+
+            if self.sound.compararDEE(r'Records\left.wav'):
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT))
+
+
+
+    def start_recording(self):
+        self.recording = True
+        self.start()
+
+    def stop_recording(self):
+        self.recording = False
+        self.join()
 
 class interfaz_metodo:
+    pass
+
+class snake_game:
     pass
